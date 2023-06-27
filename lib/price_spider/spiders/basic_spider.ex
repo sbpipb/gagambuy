6e63553435cff76461aead2f79dacfecc1dbd530
sbpipb <sbpipb>
2023-06-28 00:27:03 +0800

    credo fix

--- lib/price_spider/spiders/basic_spider.ex
+++ lib/price_spider/spiders/basic_spider.ex
@@ -1,4 +1,7 @@
 defmodule PriceSpider.BasicSpider do
+  @moduledoc """
+  This module is to crawl maap products for update archive sale prices
+  """
   use Crawly.Spider
   @impl Crawly.Spider
 
@@ -7,7 +10,10 @@ defmodule PriceSpider.BasicSpider do
   @impl Crawly.Spider
   def init() do
     [
-      start_urls: [ "https://maap.cc/collections/archive-sale-man" ]
+      start_urls: [
+        "https://maap.cc/collections/archive-sale-man",
+        "https://maap.cc/collections/archive-sale-woman"
+      ]
     ]
   end
 
@@ -18,19 +24,16 @@ defmodule PriceSpider.BasicSpider do
     
     products = response.body |> Floki.parse_document |> elem(1) |> Floki.find(".product_card > .productDetails > .productMeta > a")
 
-      items = products |> Enum.map(fn product -> 
+    items = products |> Enum.map(fn product -> 
+      url = product |> elem(1) |> List.last |> elem(1)
+      [product_name, price] = product |> Floki.text |> String.split("Now")
+      [sale_price, original_price] = price |> String.split(" AUD", trim: true)
 
-          url = product |> elem(1) |> List.last |> elem(1)
-          [product_name, price] = product |> Floki.text |> String.split("Now")
-          [sale_price, original_price] = price |> String.split(" AUD", trim: true)
-
-          %{url: url,
-            name: product_name,
-            price: sale_price,
-            og_price: original_price
-          }
-        end)
-
+      %{url: url,
+        name: product_name,
+        price: sale_price,
+        og_price: original_price}
+    end)
 
     %Crawly.ParsedItem{
       :items => items,
