87589a6cc90dd2ff04da3328680e7ea6a4ba8891
sbpipb <sbpipb>
2023-06-27 23:57:53 +0800

    Update config file

--- config/config.exs
+++ config/config.exs
@@ -1,11 +1,22 @@
 import Config
 
 config :crawly,
-    middlewares: [
-        {Crawly.Middlewares.UserAgent, user_agents: [
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
-        ]},
+  closespider_timeout: 10,
+  concurrent_requests_per_domain: 8,
+  closespider_itemcount: 100,
 
-        Crawly.Pipelines.WriteToFile
-    ]
+  middlewares: [
+    Crawly.Middlewares.DomainFilter,
+    Crawly.Middlewares.UniqueRequest,
+    {Crawly.Middlewares.UserAgent, user_agents: ["Crawly Bot", "Google"]}
+  ],
+  pipelines: [
+    # An item is expected to have all fields defined in the fields list
+    {Crawly.Pipelines.Validate, fields: [:url, :name, :og_price, :price]},
 
+    # Use the following field as an item uniq identifier (pipeline) drops
+    # items with the same urls
+    {Crawly.Pipelines.DuplicatesFilter, item_id: :url},
+    Crawly.Pipelines.JSONEncoder,
+    {Crawly.Pipelines.WriteToFile, extension: "jl", folder: "/tmp"}
+  ]
